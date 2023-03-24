3622bc62ed29ba8c1bb442ed8cafd25d2c59b8c1
Krzysztof A. Kluza <krzysztof.a.kluza@gmail.com>
2023-03-24 13:51:31 +0100

    tutorial for(of)

--- js/script.js
+++ js/script.js
@@ -1,27 +1,38 @@
 'use strict';
 
-/*
+/* written by me for tests and suprise... don't work
+
 document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
   });
 */
 
+//pasted from Kodilla, added console.log(event), added console.log(link),
+const titleClickHandler = function(event){
+    console.log('Link was clicked!', event);
 
-const titleClickHandler = function(){
-    console.log('Link was clicked!');
-    // console.log('event');
+    /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+}
+
+    /* add class 'active' to the clicked link */
+
+    /* remove class 'active' from all articles */
+
+    /* get 'href' attribute from the clicked link */
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
   }
-
-  // remove class 'active' from all article links
-  // add class 'active' to the clicked link
-  // remove class 'active' from all articles
-  // get 'href' attribute from the clicked link
-  // find the correct article using the selector (value of 'href' attribute)
-  // add class 'active' to the correct article
 
   const links = document.querySelectorAll('.titles a');
 
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
+    console.log(link);
   }
