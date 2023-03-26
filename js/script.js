d4ea6160f1f9c8f6b45a3c286ea3f07abf493138
Krzysztof A. Kluza <krzysztof.a.kluza@gmail.com>
2023-03-26 19:13:34 +0200

    const activeArticles,querySelectorAll changes

--- js/script.js
+++ js/script.js
@@ -1,42 +1,50 @@
-'use strict';
+{
+  //use Strict Mode
+  'use strict';
 
-/* written by me for tests and suprise... don't work
-
-document.getElementById('test-button').addEventListener('click', function(){
+  /*
+  document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
-  });
-  */
+  }); */
 
-
-//pasted from Kodilla, added console.log(event), added console.log(link),
-const titleClickHandler = function(event){
-    console.log('Link was clicked!', event);
-}
-
-//remove class 'active' from all article links
-const activeLinks = document.querySelectorAll('.titles a.active');
-//dlaczego szukam w dokumencie .titles oraz a.active, skoro klasa to active
-for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
-}
-
-
-/* remove class 'active' from all articles */
-const activeArticles  = document.querySelectorAll('.titles a.active');
-for(let activeArticle of activeArticles){
-activeLink.classList.remove('active');
-
-/* get 'href' attribute from the clicked link */
-
-/* find the correct article using the selector (value of 'href' attribute) */
-
-/* add class 'active' to the correct article */
-  } //koniec funkcji remove 'active'
-
-const links = document.querySelectorAll('.titles a');
-
-for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-    console.log(link);
+  const titleClickHandler = function(event){
+  console.log('Link was clicked!', event);
   }
+
+  //remove class 'active' from all article links
+
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+  }
+
+  /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.post-title post-author');
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
+}
+
+
+
+  /* remove class 'active' from all articles */
+
+
+
+ // activeLink.classList.remove('active');
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+    //koniec funkcji remove 'active'
+
+     //ponizsze chyba do usniecia, bo kod zle dziala z tym
+     /*
+  const links = document.querySelectorAll('.titles a');
+  for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+      console.log(link);
+    }
+    */
