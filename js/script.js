6a58969c4ef4af76c895b79e7514a803535fd1ac
Krzysztof A. Kluza <krzysztof.a.kluza@gmail.com>
2023-03-27 10:44:40 +0200

    script formated

--- js/script.js
+++ js/script.js
@@ -9,26 +9,40 @@
   }); */
 
   const titleClickHandler = function(event){
-  console.log('Link was clicked!', event);
+    const clickedElement = this;
+    console.log('Link was clicked!', event);
   }
 
-  //remove class 'active' from all article links
+  //[DONE] remove class 'active' from all article links
 
   const activeLinks = document.querySelectorAll('.titles a.active');
   for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
   }
 
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.post-title post-author');
+  //[IN PROGRESS] remove class 'active' from all articles
+  const activeArticles = document.querySelectorAll('post-title post-author');
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
+
+  //click event handler
+  const clickedElement = this;
+    activeLink.classList.add('active');
+
+  //[IN PROGRESS] add class 'active' to the clicked link
+  console.log('clickedElement:', clickedElement);
+
+
+//add class 'active' to the correct article
+  const links = document.querySelectorAll('.titles a');
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+    console.log(link);
+  }
+
 }
 
-
-
-  /* remove class 'active' from all articles */
 
 
 
@@ -42,9 +56,5 @@
 
      //ponizsze chyba do usniecia, bo kod zle dziala z tym
      /*
-  const links = document.querySelectorAll('.titles a');
-  for(let link of links){
-      link.addEventListener('click', titleClickHandler);
-      console.log(link);
-    }
+
     */
