7049e9544745b08037dbab6807a7fe5922a5e4a0
Krzysztof A. Kluza <krzysztof.a.kluza@gmail.com>
2023-03-28 10:06:28 +0200

    code format

--- js/script.js
+++ js/script.js
@@ -17,32 +17,32 @@
     //select .titles a
     const links = document.querySelectorAll('.titles a');
 
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-    console.log(link);
-  }
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+      console.log(link);
+    }
 
-  //[DONE] what is activeLink
-  const activeLink = titleClickHandler(document.querySelectorAll('.titles a.active'));
+    //[DONE] what is activeLink
+    const activeLink = titleClickHandler(document.querySelectorAll('.titles a.active'));
 
-  //[DONE] remove class 'active' from all article links
+    //[DONE] remove class 'active' from all article links
 
-  const activeLinks = document.querySelectorAll('.titles a.active');
-  for(let activeLink of activeLinks){
-      activeLink.classList.remove('active');
-  }
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
 
 
-  //[DONE] remove class 'active' from all articles
-  const activeArticles = document.querySelectorAll('.post-title .post-author');
-  for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-  }
+    //[DONE] remove class 'active' from all articles
+    const activeArticles = document.querySelectorAll('.post-title .post-author');
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+    }
 
-  //[IN PROGRESS] add a class 'active' to the clicked link
-  this.classList.add('active');
-  //clickedElement.classList.add('active');
-  console.log('clickedElement:', this);
+    //[IN PROGRESS] add a class 'active' to the clicked link
+    this.classList.add('active');
+    //clickedElement.classList.add('active');
+    console.log('clickedElement:', this);
 
 
   }
