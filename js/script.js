2c5946563b50baacbf936fbf30c7f974eff701c0
Krzysztof A. Kluza <krzysztof.a.kluza@gmail.com>
2023-03-30 18:03:03 +0200

    [IN PROGRESS] generate list title

--- js/script.js
+++ js/script.js
@@ -38,26 +38,100 @@ const titleClickHandler = function(event) {
   console.log('targetArticle:', targetArticle);
 
 
-  /* [IN PROGRESS] add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
   targetArticle.classList.add('active');
 
+  /* [IN PROGRESS] define sript options */
+  //opt means options
+  /*
+
+  const optArticleSelector = '.post',
+    optTitleSelector = 'post-title',
+    optTitleListSelector = '.titles';
+
+
+
+  /*
   /* [IN PROGRESS] generate list title for articles */
+
   // function generateTitleLinks(getElementById(article*)) {}
-  /* created HTML is HTML + const(with id article) */
+  /* created HTML is HTML + const(with id article)
+  const Article-1 = {
+    title: "Article 1",
+    autor: Marion Berry,
+    id: "article-1"
+  }
+
+  const Article-2 = {
+    title: "Article 2 ",
+    autor: Theo Tabby,
+    id: "article-2"
+  }
+
+  const Article-3 = {
+    title: "Article 3 ",
+    autor: George Tuxedo,
+    id: "article-3"
+  }
+
+  const Article-4 = {
+    title: "Article 4",
+    autor: Kitty Toebean,
+    id: "article-4"
+  }
+
+  const Article-5 = {
+    title: "Article 5",
+    autor: Marion Berry,
+    id: "article-5"
+  }
+
+  const Article-6 = {
+    title: "Article 6",
+    autor: George Tuxedo,
+    id: "article-6"
+  }
+
+  const Article-7 = {
+    title: "Article 7",
+    autor: Kitty Toebean,
+    id: "article-7"
+  }
+
+  const Article-8 = {
+    title: "Article 8",
+    autor: Theo Tabby,
+    id: "article-8"
+  }
+
+  const Article-9 = {
+    title: "Article 9",
+    autor: Marion Berry,
+    id: "article-9"
+  }
+
+  const Article-10 = {
+    title: "Article 10",
+    autor: George Tuxedo,
+    id: "article-10"
+  }
 
   //remove list titles
 
   //find articles "id" and save it as const
 
-  //finf articles titles and save it as const
+  //find articles titles and save it as const
   //querySelector(title)
 
   //based on "id" and titles create html code and save it const
   //use innerHTML to change htnl doc content, "read" attributes using getAttribute
 
   //put the code at list title column | use innerHTML to add link to link list
+  */
 
 }
+
+
 
 const links = document.querySelectorAll('.titles a');
 
