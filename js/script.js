a007aec50e9c57871347156b2fc65995d6e9561e
Krzysztof A. Kluza <krzysztof.a.kluza@gmail.com>
2023-04-05 13:47:09 +0200

    code amended.left column(working)

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@
   //Strict Mode
   'use strict';
 
-
-
+  function titleClickHandler() {
+    const clickedElement = this;
     /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
-      for(let activeLink of activeLinks){
+    for (let activeLink of activeLinks) {
       activeLink.classList.remove('active');
     }
 
@@ -19,9 +19,9 @@
     /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.post.active');
-      for (let activeArticle of activeArticles) {
-        activeArticle.classList.remove('active');
-      }
+    for (let activeArticle of activeArticles) {
+      activeArticle.classList.remove('active');
+    }
 
     /* [DONE] get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
@@ -36,66 +36,51 @@
 
     /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
+  }
 
-    /* [DONE] define sript options */
-    const optArticleSelector = '.post',
-      optTitleSelector = '.post-title',
-      optTitleListSelector = '.titles';
+  /* [DONE] define sript options */
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
 
-    function generateTitleLinks(){
+  function generateTitleLinks() {
 
     /* [DONE] remove contents of titleList */
     const titleList = document.querySelector(optTitleListSelector);
     titleList.innerHTML = '';
 
-     /* [DONE] Create a single article */
-     const article = optArticleSelector;
+    /* [DONE] Create a single article */
+    const article = optArticleSelector;
 
     /* [DONE] for each article */
     const articles = document.querySelectorAll(optArticleSelector);
 
-    /* Variable scope */
-    let html = '';
+    for (let article of articles) {
+      /* [IN PROGRESS] get the article id */
+       const articleId = article.getAttribute('id');
 
-    for(let article of articles){
-      console.log('article && html');
-    }
+      console.log(articleId);
 
-    /* [IN PROGRESS] get the article id */
-    const articleId = article.getAttribute('href');
-    console.log(articleId);
+      /* [DONE] find the title element */
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      //What it does? Create a constant value which look for - article - class .post-title. Search target: html
 
-    /* [DONE] find the title element */
-    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-    //What it does? Create a constant value which look for - article - class .post-title. Search target: html
+      /* [DONE] get the title from the title element */
 
-    /* [DONE] get the title from the title element */
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      console.log('linkHTML');
 
-    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-    console.log('linkHTML');
+      /* create HTML of the link */
+      titleList.innerHTML = titleList.innerHTML + linkHTML;
 
-    /* create HTML of the link */
-    titleList.innerHTML = titleList.innerHTML + linkHTML;
-
-
-    /* insert link into titleList */
-
-    const titleClickHandler = function(event) {
-    event.preventDefault();
-    const clickedElement = this;
-    console.log('Link was clicked!', event);
+      const links = document.querySelectorAll('.titles a');
+      for (let link of links) {
+        link.addEventListener('click', titleClickHandler);
+      }
 
     }
-
-    generateTitleLinks();
-
   }
 
-
-
-  const links = document.querySelectorAll('.titles a');
-    for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-    }
+  generateTitleLinks();
 
 }
