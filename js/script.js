91a2b90aeec46520565f4863cd17c6727431e68f
Krzysztof A. Kluza <krzysztof.a.kluza@gmail.com>
2023-05-15 11:13:58 +0200

    add create a constant an allTagsData

--- js/script.js
+++ js/script.js
@@ -224,7 +224,10 @@
     const tagList = document.querySelector(select.listOf.tags);
 
     /* [NEW] create variable for all links HTML code */
-    let allTagsHTML = '';
+    //let allTagsHTML = '';
+
+    /* create a constant an allTagsData with an tag object */
+    const allTagData = {tags: []};
 
     /* [NEW] using tags to change display */
 
